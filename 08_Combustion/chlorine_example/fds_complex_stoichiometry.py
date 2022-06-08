88234d5261bde99124fc77a33adf9ab76ad466fe
rmcdermo <randy.mcdermott@gmail.com>
2022-06-08 08:25:27 -0400

    Combustion: update stoich python tools

--- 08_Combustion/chlorine_example/fds_complex_stoichiometry.py
+++ 08_Combustion/chlorine_example/fds_complex_stoichiometry.py
@@ -3,13 +3,8 @@
 # 08-02-2017
 # fds_complex_stoichiometry.py
 
-from __future__ import division # make floating point division default as in Matlab, e.g., 1/2=0.5
-import math
 import numpy as np
-import scipy.special as sp
 import matplotlib.pyplot as plt
-from matplotlib import rc
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'],'size':16})
 
 # define atomic weights
 
