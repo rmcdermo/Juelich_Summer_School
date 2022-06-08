97459d442bbbffcec850877c56af9d62f8f7bcd8
rmcdermo <randy.mcdermott@gmail.com>
2022-06-08 06:02:18 -0400

    Combustion: update hrr plot in finite rate

--- 08_Combustion/finite_rate_example/plot_hrr.py
+++ 08_Combustion/finite_rate_example/plot_hrr.py
@@ -2,21 +2,17 @@
 #McDermott
 #2016-02-01 20:52:39
 
-from __future__ import division # make floating point division default as in Matlab, e.g., 1/2=0.5
-import math
+import pandas as pd
 import numpy as np
-import scipy.special as sp
 import matplotlib.pyplot as plt
-from matplotlib import rc
-rc('font',**{'family':'sans-serif','sans-serif':['Helvetica'],'size':16})
 
 np.seterr(divide='ignore', invalid='ignore')
 
 # read data from _devc file
-M = np.genfromtxt('wd_propane_hrr.csv', delimiter=',', skip_header=1, names=True)
+M = pd.read_csv('wd_propane_hrr.csv', header=1)
 t = M['Time']
 HRR      = M['HRR']
-MLR_FUEL = M['MLR_FUEL']
+MLR_FUEL = M['MLR_PROPANE']
 HOC  = 46334 # kJ/kg
 AREA = 1     # m^2
 
@@ -32,7 +28,6 @@ plt.plot(t,ETA, label='', **marker_style_1)
 plt.axis([min(t), max(t), 0, 2])
 plt.xlabel('time (s)')
 plt.ylabel('$\eta$ = Q / (MLR $\\times$ HOC $\\times$ A)')
-plt.legend(loc='lower right', numpoints=1, frameon=False)
 #plt.show()
 plt.savefig('comb_efficiency.pdf', format='pdf')
 plt.close()
