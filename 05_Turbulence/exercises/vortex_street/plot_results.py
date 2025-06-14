fb05631fef9831851e15e8db1230a768d790d960
Lukas <lu-kas@users.noreply.github.com>
2025-06-14 19:29:27 +0200

    work on vortex street example, added input files and results images

---
+++ 05_Turbulence/exercises/vortex_street/plot_results.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import os
+
+import fdsreader
+
+rdirs = glob.glob("rundir/nu*")
+
+for d in rdirs:
+    nu = os.path.split(d)[-1]
+
+    sim = fdsreader.Simulation(d)
+    slc = sim.slices.filter_by_quantity("VORTICITY Y")[0]
+    slc_data = slc.to_global(masked=True, fill=np.nan)
+
+    it = slc.get_nearest_timestep(2)
+    # visualise the data
+    plt.imshow(slc_data[it,:,:].T, 
+               vmax=500,
+               vmin=-500,
+               cmap='seismic',
+               origin='lower', 
+               extent=slc.extent.as_list())
+
+    # add labels
+    plt.title(f'nu = {nu}; y-vorticity at t={slc.times[it]:.2f}s')
+    plt.xlabel('x / m')
+    plt.ylabel('z / m')
+    plt.colorbar(orientation='horizontal', label=f'{slc.quantity.name} / {slc.quantity.unit}' )
+
+    # save output to file
+    plt.savefig(f'result_{nu}.png', bbox_inches='tight', dpi=200)
+    plt.clf()
+
---
+++ 05_Turbulence/exercises/vortex_street/rundir/run.sh
@@ -0,0 +1,17 @@
+mkdir nu_12e-6
+cp vortex_street_nu12e-6.fds nu_12e-6
+cd nu_12e-6
+fds *.fds
+cd -
+
+mkdir nu_12e-5
+cp vortex_street_nu12e-5.fds nu_12e-5
+cd nu_12e-5
+fds *.fds
+cd -
+
+mkdir nu_6e-6
+cp vortex_street_nu6e-6.fds nu_6e-6
+cd nu_6e-6
+fds *.fds
+cd -
