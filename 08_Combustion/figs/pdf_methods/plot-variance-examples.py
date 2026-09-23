53665b0cd2ddb4a02e16dffea00963b1d2b5a15a
Randy McDermott <randy.mcdermott@gmail.com>
2026-09-23 16:22:24 -0400

    Combustion: more sgs variance examples

--- 08_Combustion/figs/pdf_methods/plot-variance-examples.py
+++ 08_Combustion/figs/pdf_methods/plot-variance-examples.py
@@ -1,4 +1,5 @@
-"""Reproduce the two variance examples; symbolic axis labels live in QMD."""
+"""Reproduce the variance examples; symbolic axis labels live in QMD."""
+import argparse
 from pathlib import Path
 
 import matplotlib
@@ -6,7 +7,14 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import numpy as np
 
-for name, values in [("unmixed", (0, 1)), ("partially-mixed", (0.2, 0.8))]:
+cases = {"unmixed": (0, 1), "partially-mixed": (0.2, 0.8),
+         "nearly-mixed": (0.4, 0.6), "fully-mixed": (0.5, 0.5)}
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("--cases", nargs="+", choices=cases, default=list(cases))
+args = parser.parse_args()
+
+for name in args.cases:
+    values = cases[name]
     data = np.full((10, 10), values[0], dtype=float)
     data[:, 5:] = values[1]
     fig = plt.figure(figsize=(8, 3.5))
@@ -19,7 +27,8 @@ for name, values in [("unmixed", (0, 1)), ("partially-mixed", (0.2, 0.8))]:
     scale.tick_params(labelsize=10)
     histogram = fig.add_axes([0.51, 0.15, 0.47, 0.8])
     histogram.hist(data.ravel(), bins=np.linspace(0, 1, 100), color="blue")
-    histogram.set(xlim=(0, 1), ylim=(0, 50), xticks=np.linspace(0, 1, 6))
+    histogram.set(xlim=(0, 1), ylim=(0, 100 if values[0] == values[1] else 50),
+                  xticks=np.linspace(0, 1, 6))
     histogram.tick_params(direction="in", labelsize=11)
     for ax in (cell, histogram, scale):
         for spine in ax.spines.values():
